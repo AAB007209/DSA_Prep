5679a1f8d6c537e0934235825a51f3f21061303c
AKASH A BENKI <akash.0.benki74169@gmail.com>
2024-10-21 19:44:46 +0530

    Added one more function

--- TheSkyFire/JS/Longest_Common_Prefix.js
+++ TheSkyFire/JS/Longest_Common_Prefix.js
@@ -7,7 +7,7 @@
 // Time Complexity - O(N * M); // N = strs length, M = each str in strs length in worst case (Longest word)
 // Space Complexity - O(M)
 
-function longestCommonPrefix(strs) {
+function longestCommonPrefix1(strs) {
 
     let prefix = strs[0];
 
@@ -22,6 +22,25 @@ function longestCommonPrefix(strs) {
     return prefix;
 }
 
+function longestCommonPrefix2(strs) {
+    let refString = strs[0];
+
+    for (let i = 1; i < strs.length; i++) {
+        let currString = strs[i];
+        let j = 0;
+        while (j < refString.length && j < currString.length && refString[j] === currString[j]) {
+            j++;
+        }
+
+        refString = refString.slice(0, j);
+    }
+    return refString;
+}
+
 const strs = ["flower", "flow", "flight"];
-const answer = longestCommonPrefix(strs);
+const answer = longestCommonPrefix1(strs);
 console.log(answer); // Output - fl
+
+const strs2 = ["Rocket", "Rock", "Rome"];
+const answer2 = longestCommonPrefix2(strs2);
+console.log(answer2); // Output - Ro
