1d62aa662c17c75a6ac6491c65585e90c0f3fd05
AKASH A BENKI <akash.0.benki74169@gmail.com>
2024-10-28 23:57:43 +0530

    Addedone more function

--- TheSkyFire/Basic_Maths/Count_Digits.js
+++ TheSkyFire/Basic_Maths/Count_Digits.js
@@ -25,10 +25,31 @@ function countDigits(N) {
     return counter;
 }
 
+// - Optimal Solution
+// Time Complexity - O(D) -> D is the No.of Digits in N String
+// Space Complexity - O(1)
+
+function countDigits2(N) {
+    let numCount = 0
+    let numLoop = N
+    while (numLoop !== 0) {
+        let newNum = Math.floor(numLoop % 10)
+        numLoop = Math.floor(numLoop / 10)
+        if (newNum !== 0 && N % newNum === 0) {
+            numCount++
+        }
+    }
+    return numCount;
+}
+
 // - Driver code
 console.log(countDigits(12)); // Output -> 2
 console.log(countDigits(2446)); // Output -> 1
 console.log(countDigits(23)); // Output -> 0
+
+console.log(countDigits2(12)); // Output -> 2
+console.log(countDigits2(2446)); // Output -> 1
+console.log(countDigits2(23)); // Output -> 0
 
 // Explanation of the outputs
 /*
@@ -36,3 +57,21 @@ console.log(countDigits(23)); // Output -> 0
 2. For 2446: Output is 1 because the among digits 2, 4 and 6 only 2 divides it evenly.
 3. For 23: Output is 0 because digits 2 and 3 doesn't divide 23 evenly.
 */
+
+/* 
+-> Some points for learning
+
+let digits = Array.from(String(N), Number);
+
+Breakdown :
+-> String(N);             // "1234"
+-> Array.from("1234");    // ["1", "2", "3", "4"]
+-> Array.from("1234", Number); // [1, 2, 3, 4]
+
+Array.from(String(N), Number) – Creates an array from the string representation of N:
+
+Array.from() takes two arguments: the first is an iterable (like a string), and the second is a mapping function that operates on each element of the iterable.
+Here, Number is used as the mapping function, which converts each character (digit) back to a number.
+
+*/
+
