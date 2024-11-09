39834379fea921cb18ff7e78e7acd37dc0f14ca5
AKASH A BENKI <akash.0.benki74169@gmail.com>
2024-11-09 15:13:17 +0530

    Updated Moore's Algo Optimal Solution

--- TheSkyFire/Arrays/Majority_Element_2.js
+++ TheSkyFire/Arrays/Majority_Element_2.js
@@ -63,12 +63,97 @@ function majorityElement2(nums) {
     return arr;
 }
 
-// - Optimal Solution (Yet to be understood and updated)
+// - Optimal Solution (Modified Moore's Voting Algorithm)
 // Time Complexity - O(N)
 // Space Complexity - O(1)
 
+function majorityElement3(nums) {
+    let n = nums.length;
+    if (n === 1) {
+        return nums;
+    }
+
+    let cnt1 = 0, cnt2 = 0;
+    let el1 = Number.MIN_VALUE;
+    let el2 = Number.MIN_VALUE;
+    let arr = [];
+
+    for (let i = 0; i < n; i++) {
+        if (cnt1 === 0 && el2 !== nums[i]) {
+            cnt1 = 1;
+            el1 = nums[i];
+        }
+        else if (cnt2 === 0 && el1 !== nums[i]) {
+            cnt2 = 1;
+            el2 = nums[i];
+        }
+        else if (nums[i] === el1) {
+            cnt1++;
+        }
+        else if (nums[i] === el2) {
+            cnt2++;
+        }
+        else {
+            cnt1--;
+            cnt2--;
+        }
+    }
+
+    cnt1 = 0, cnt2 = 0;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === el1) {
+            cnt1++;
+        }
+        if (nums[i] === el2) {
+            cnt2++;
+        }
+    }
+
+    let condition = Math.floor(n / 3);
+    if (cnt1 > condition) arr.push(el1);
+    if (cnt2 > condition) arr.push(el2);
+    return arr;
+}
+
 // - Driver code
 console.log(majorityElement1([11, 33, 33, 11, 33, 11])); // [11, 33]
-console.log(majorityElement2([3, 2, 3])); // [3]
 console.log(majorityElement1([1])); // [1]
+
+console.log(majorityElement2([3, 2, 3])); // [3]
 console.log(majorityElement2([1, 2])); // [1, 2]
+
+console.log(majorityElement3([2, 1, 1, 3, 1, 4, 5, 6])); // [1]
+console.log(majorityElement3([1, 3, 3, 4, 3, 6])); // [3]
+
+
+
+
+
+// - New Learning
+/*
+- We can also use Object as Map to store the Frequencies of the elements: O(N), O(N);
+
+var majorityElement = function(nums) {
+
+    if(nums.length === 1){
+        return nums;
+    }
+
+    obj = {}
+    for(i of nums){
+        if(!obj[i]){
+            obj[i]=1
+        }else{
+            obj[i]++
+        }
+    }
+    v=[]
+    for(const i in obj){
+        if(obj[i]>Math.floor(nums.length/3)){
+         v.push(Number(i))
+        }
+    }
+    return v
+};
+
+*/
