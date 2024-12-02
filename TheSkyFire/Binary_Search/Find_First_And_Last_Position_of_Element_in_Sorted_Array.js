bbe0470c171f94ff355a580134d35c86e84708bc
AKASH A BENKI <akash.0.benki74169@gmail.com>
2024-12-03 00:22:55 +0530

    Adding Find First and Last Position of an Element

---
+++ TheSkyFire/Binary_Search/Find_First_And_Last_Position_of_Element_in_Sorted_Array.js
@@ -0,0 +1,82 @@
+// - Problem Description (Leetcode: 34. Find First and Last position of element in sorted Array)
+
+// Link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+// - Bruteforce Approach (Linear Search)
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+
+function findFirstAndLast1(nums, target) {
+    let n = nums.length;
+    if (n === 0) return [-1, -1];
+
+    let first = last = -1;
+
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === target) {
+            if (first === -1 && last === -1) {
+                first = last = i;
+            }
+            else {
+                last = Math.max(last, i);
+            }
+        }
+    }
+    return [first, last];
+}
+
+// - Optimal Approach (Binary Search)
+// Time Complexity - O(Log N)
+// Space Complexity - O(1)
+
+function findFirstAndLast2(nums, target) {
+    let n = nums.length;
+    if (n === 0) return [-1, -1];
+    if (n === 1 && nums[0] === target) return [0, 0];
+
+    let startIndex = -1, endIndex = -1;
+    let left = 0, right = n - 1;
+    while (left <= right) {
+        let mid = (left + right) >> 1;
+        if (nums[mid] === target) {
+            startIndex = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    left = 0, right = n - 1;
+    while (left <= right) {
+        let mid = (left + right) >> 1;
+        if (nums[mid] === target) {
+            endIndex = mid;
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return [startIndex, endIndex];
+}
+
+// - Driver code
+// Bruteforce
+console.log(findFirstAndLast1([5, 7, 7, 8, 8, 10], 8));
+console.log(findFirstAndLast1([5, 7, 7, 8, 8, 10], 6));
+console.log(findFirstAndLast1([], 0));
+
+// Optimal (Binary Search)
+console.log(findFirstAndLast1([5, 7, 7, 8, 8, 10], 8));
+console.log(findFirstAndLast1([5, 7, 7, 8, 8, 10], 6));
+console.log(findFirstAndLast1([], 0));
