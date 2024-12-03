db1669826f16a94e32d08fa299036c5176213b9c
AKASH A BENKI <akash.0.benki74169@gmail.com>
2024-12-03 23:33:32 +0530

    Updated little conditions

--- TheSkyFire/Binary_Search/Search_in_Rotated_Sorted_Array.js
+++ TheSkyFire/Binary_Search/Search_in_Rotated_Sorted_Array.js
@@ -34,13 +34,13 @@ function search2(nums, target) {
 
         if (nums[mid] === target) return mid;
         else if (nums[mid] >= nums[left]) {
-            if (nums[left] <= target && target <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         } else {
-            if (nums[mid] <= target && target <= nums[right]) {
+            if (nums[mid] < target && target <= nums[right]) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
