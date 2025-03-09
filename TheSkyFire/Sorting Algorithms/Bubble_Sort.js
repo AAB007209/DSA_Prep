c8e360a58cb958ed64fa33f333866b695e209d92
AKASH A BENKI <akash.0.benki74169@gmail.com>
2025-03-09 11:42:57 +0530

    Adding Bubble Sort

---
+++ TheSkyFire/Sorting Algorithms/Bubble_Sort.js
@@ -0,0 +1,26 @@
+// This is the Program for Bubble Sort
+
+// Time Complexity - O(N^2)
+// Space Complexity - O(1)
+
+function bubbleSort(arr) {
+    const n = arr.length;
+    let flag = true;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                flag = false;
+            }
+        }
+        if (flag === true) {
+            console.log("Array is already Sorted");
+            return arr;
+        }
+    }
+    return arr;
+}
+
+// - Driver code
+console.log(bubbleSort([4, 1, 5, 2, 3]));
+console.log(bubbleSort([1, 2, 3, 4, 5]));
