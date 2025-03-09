9a1a99fa658851dca837641d0c72d85f70b07a3b
AKASH A BENKI <akash.0.benki74169@gmail.com>
2025-03-09 12:15:38 +0530

    Adding Selection Sortg

---
+++ TheSkyFire/Sorting Algorithms/Selection_Sort.js
@@ -0,0 +1,24 @@
+// This is the Program for Selection Sort
+
+// Time Complexity - O(N^2)
+// Space Complexity - O(1)
+
+function selectionSort(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let smallestIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[smallestIndex]) {
+                smallestIndex = j;
+            }
+        }
+
+        // Swap the smallestIndex element with the first element of the unsorted.
+        [arr[i], arr[smallestIndex]] = [arr[smallestIndex], arr[i]];
+    }
+    return arr;
+}
+
+// - Driver Code
+console.log(selectionSort([4, 1, 2, 5, 3, 8, 6, 7]));
+console.log(selectionSort([100, 30, 50, 10, 20, 40, 90, 80, 70, 60]));
